cebc0001fb98306fdc89411bc87fe5d6672a6111
Anjali261 <mail2anjali.singh2610@gmail.com>
2023-07-02 21:52:00 +0530

    Server Setup and Database  Connection

---
+++ Backend/index.js
@@ -0,0 +1,32 @@
+const express  = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.set('strictQuery', false);
+const connectDB = async() =>{
+    try{
+        const con = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+
+        })
+        console.log(`MongoDB connected: ${con.connection.host}`);
+        
+    }catch(error){
+        console.log(error);
+        process.exit(1);
+
+    }
+}
+connectDB();
+
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Server is listening at http://localhost:${process.env.PORT}`);
+})
