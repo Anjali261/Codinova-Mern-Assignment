ec0279e9099b5067036b271f2b0e2a599308ddbf
Anjali261 <mail2anjali.singh2610@gmail.com>
2023-07-03 03:01:28 +0530

    Created Pagination and search filter & debuged in backend code

--- Backend/index.js
+++ Backend/index.js
@@ -1,43 +1,176 @@
-const express  = require('express');
+import express from 'express';
+import axios from 'axios';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import pgp from 'pg-promise';
+
 const app = express();
-
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-
 mongoose.set('strictQuery', false);
-const connectDB = async() =>{
-    try{
-        const con = await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser:true,
 
-        })
-        console.log(`MongoDB connected: ${con.connection.host}`);
-        
-    }catch(error){
-        console.log(error);
-        process.exit(1);
+const connectDB = async () => {
+  try {
+    const con = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${con.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-    }
-}
 connectDB();
 
 const exchangeSchema = new mongoose.Schema({
-    name: String,
-    symbol: String,
-    iconUrl: String,
-  });
-  
-  const Exchange = mongoose.model('Exchange', exchangeSchema);
-  
+  exchange_id: String,
+  name: String,
+  website: String,
+  icon: String,
+});
+
+const Exchange = mongoose.model('Exchange', exchangeSchema);
 
 
 
 
-app.listen(process.env.PORT, () =>{
-    console.log(`Server is listening at http://localhost:${process.env.PORT}`);
-})
+
+// Fetch and store exchange data
+app.get('/fetch-exchanges', async (req, res) => {
+  try {
+    const apiKey = `0D690090-8231-403E-BC3D-700E500A7034`; 
+
+    // Fetch exchange data from CoinAPI.io
+    const exchangesResponse = await fetch(`https://rest.coinapi.io/v1/exchanges/apikey-${apiKey}`);
+
+    const exchangesData = await exchangesResponse.json();
+
+    // Ensure exchangesData is an array
+    const exchanges = Array.isArray(exchangesData) ? exchangesData : [];
+
+    // Fetch exchange icons and map them to exchanges
+    const iconsResponse = await fetch(`https://rest.coinapi.io/v1/exchanges/icons/32/apikey-${apiKey}`);
+
+    const iconsData = await iconsResponse.json();
+
+    // Ensure iconsData is an array
+    const icons = Array.isArray(iconsData) ? iconsData : [];
+
+    const exchangesWithIcons = exchanges.map((exchange) => {
+      const icon = icons.find((icon) => icon.exchange_id === exchange.exchange_id);
+      return {
+        exchange_id: exchange.exchange_id,
+        name: exchange.name,
+        website: exchange.website,
+        icon: icon ? icon.url : null,
+      };
+    });
+
+    // Store exchange data in the database
+    await Exchange.deleteMany();
+    await Exchange.insertMany(exchangesWithIcons);
+
+    console.log('Fetched and stored exchange data:');
+    console.log(exchangesWithIcons);
+
+    res.status(200).json({ message: 'Exchange data fetched and stored successfully.' });
+  } catch (error) {
+    console.error('Error fetching and storing exchange data:', error);
+    res.status(500).json({ error: 'Failed to fetch and store exchange data.' });
+  }
+});
+
+// ExchangeList API to fetch exchange data from the database
+app.get('/exchange-list', async (req, res) => {
+  try {
+    const exchanges = await Exchange.find();
+    res.status(200).json(exchanges);
+  } catch (error) {
+    console.error('Error fetching exchange data:', error);
+    res.status(500).json({ error: 'Failed to fetch exchange data.' });
+  }
+});
+
+
+
+// // Fetch and store exchange data
+// app.get('/fetch-exchanges', async (req, res) => {
+//   try {
+//     const apiKey = 'process.env.CRYPTO_API'; 
+
+//     // Fetch exchange data from Coinapi.io
+//     const exchangesResponse = await fetch('https://rest.coinapi.io/v1/exchanges', {
+//       headers: {
+//         'X-CoinAPI-Key': apiKey,
+//       },
+//     });
+
+//     const exchanges = await exchangesResponse.json();
+
+//     // Fetch exchange icons and map them to exchanges
+//     const iconsResponse = await fetch('https://rest.coinapi.io/v1/exchanges/icons/32', {
+//       headers: {
+//         'X-CoinAPI-Key': apiKey,
+//       },
+//     });
+
+//     const icons = await iconsResponse.json();
+
+//     const exchangesWithIcons = exchanges.map((exchange) => {
+//       const icon = icons.find((icon) => icon.exchange_id === exchange.exchange_id);
+//       return {
+//         exchange_id: exchange.exchange_id,
+//         name: exchange.name,
+//         website: exchange.website,
+//         icon: icon ? icon.url : null,
+//       };
+//     });
+
+//     // Store exchange data in the database
+//     await Exchange.deleteMany();
+
+//     for (const exchange of exchangesWithIcons) {
+//       const newExchange = new Exchange(exchange);
+//       await newExchange.save();
+//     }
+
+//     console.log('Fetched and stored exchange data:');
+//     console.log(exchangesWithIcons);
+
+//     res.status(200).json({ message: 'Exchange data fetched and stored successfully.' });
+//   } catch (error) {
+//     console.error('Error fetching and storing exchange data:', error);
+//     res.status(500).json({ error: 'Failed to fetch and store exchange data.' });
+//   }
+// });
+
+// // ExchangeList API to fetch exchange data from the database
+// app.get('/exchange-list', async (req, res) => {
+//   try {
+//     const exchanges = await Exchange.find();
+//     res.status(200).json(exchanges);
+//   } catch (error) {
+//     console.error('Error fetching exchange data:', error);
+//     res.status(500).json({ error: 'Failed to fetch exchange data.' });
+//   }
+// });
+
+    
+
+
+      
+
+
+
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import axios from 'axios';
+import { API } from './backend';
+import './App.css';
 
 const ExchangeList = () => {
   const [exchanges, setExchanges] = useState([]);
+  const [filteredExchanges, setFilteredExchanges] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [exchangesPerPage] = useState(10);
+
+  // Get the current URL query parameters
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const pageParam = params.get('page');
+  const searchParam = params.get('search');
 
   useEffect(() => {
     const fetchExchanges = async () => {
       try {
-        const response = await axios.get('/exchange-list');
+        const response = await axios.get(`${API}/exchange-list`);
         setExchanges(response.data);
       } catch (err) {
         console.error('Error fetching exchange list:', err);
@@ -20,17 +30,128 @@ const ExchangeList = () => {
     fetchExchanges();
   }, []);
 
+  useEffect(() => {
+    // Apply search filter
+    const filtered = exchanges.filter((exchange) =>
+      exchange.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    // Update filtered exchanges
+    setFilteredExchanges(filtered);
+  }, [exchanges, searchTerm]);
+
+  useEffect(() => {
+    // Update search term based on URL query parameter
+    if (searchParam) {
+      setSearchTerm(searchParam);
+    }
+  }, [searchParam]);
+
+  useEffect(() => {
+    // Update current page based on URL query parameter
+    const page = pageParam ? parseInt(pageParam) : 1;
+    setCurrentPage(page);
+  }, [pageParam]);
+
+  // Handle search input change
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  // Handle search form submit
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+
+    // Update URL query parameter
+    const searchQuery = searchTerm ? `?search=${encodeURIComponent(searchTerm)}` : '';
+    window.history.pushState({}, '', `/exchanges${searchQuery}`);
+  };
+
+  // Get current exchanges for pagination
+  const indexOfLastExchange = currentPage * exchangesPerPage;
+  const indexOfFirstExchange = indexOfLastExchange - exchangesPerPage;
+  const currentExchanges = filteredExchanges.slice(indexOfFirstExchange, indexOfLastExchange);
+
+  // Change page
+  const paginate = (pageNumber) => {
+    // Update URL query parameter
+    window.history.pushState({}, '', `/exchanges?page=${pageNumber}`);
+    setCurrentPage(pageNumber);
+  };
+
+  // Go to next page
+  const nextPage = () => {
+    if (currentPage < Math.ceil(filteredExchanges.length / exchangesPerPage)) {
+      paginate(currentPage + 1);
+    }
+  };
+
+  // Go to previous page
+  const prevPage = () => {
+    if (currentPage > 1) {
+      paginate(currentPage - 1);
+    }
+  };
+
   return (
-    <div>
-      <h1>Exchange List</h1>
-      <ul>
-        {exchanges.map((exchange) => (
-          <li key={exchange._id}>
-            <img src={exchange.iconUrl} alt={exchange.name} />
+    <div className="exchange-list-container">
+      <h1 className="exchange-list-heading">Exchange List</h1>
+
+      <form className="search-form" onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          placeholder="Search exchanges"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className="search-input"
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      <ul className="exchange-list">
+        {currentExchanges.map((exchange) => (
+          <li key={exchange._id} className="exchange-item">
+            <img src={exchange.icon} alt={exchange.name} className="exchange-icon" />
             <span>{exchange.name}</span>
           </li>
         ))}
       </ul>
+
+      <div className="pagination">
+        {/* Previous Button */}
+        {currentPage > 1 && (
+          <button className="pagination-button" onClick={prevPage}>
+            Previous
+          </button>
+        )}
+
+        {/* Page Numbers */}
+        <ul className="pagination-list">
+          {Array.from({ length: Math.ceil(filteredExchanges.length / exchangesPerPage) }).map(
+            (_, index) => (
+              <li key={index} className="pagination-item">
+                <button
+                  onClick={() => paginate(index + 1)}
+                  className={`pagination-button ${
+                    currentPage === index + 1 ? 'active' : ''
+                  }`}
+                >
+                  {index + 1}
+                </button>
+              </li>
+            )
+          )}
+        </ul>
+
+        {/* Next Button */}
+        {currentPage < Math.ceil(filteredExchanges.length / exchangesPerPage) && (
+          <button className="pagination-button" onClick={nextPage}>
+            Next
+          </button>
+        )}
+      </div>
     </div>
   );
 };
@@ -38,19 +159,27 @@ const ExchangeList = () => {
 const App = () => {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <Link to="/" className="navbar-link">
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to="/exchanges">Exchanges</Link>
+          <li className="navbar-item">
+            <Link to="/exchanges" className="navbar-link">
+              Exchanges
+            </Link>
           </li>
         </ul>
       </nav>
 
       <Routes>
-        <Route exact path="/" element={<h1>Welcome to the Crypto App!</h1>} />
+        <Route exact path="/" element={<h1 style={{backgroundColor:"black", color:"white" , textAlign:"center"}}>Welcome to the Crypto App! <br></br> Made by: <b>Anjali Kumari</b></h1>
+    
+
+      
+      } />
         <Route path="/exchanges" element={<ExchangeList />} />
       </Routes>
     </Router>
